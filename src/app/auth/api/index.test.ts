0a5852fd29c815536328b2a8fcaa7c961c18993c
Norberto Cáceres <norberto.e.888@outlook.es>
2021-02-19 09:31:19 -0500

    <trivial> formatting change

--- src/app/auth/api/index.test.ts
+++ src/app/auth/api/index.test.ts
@@ -1,28 +1,28 @@
-import request from 'supertest'
-import { validChangePasswordDto, validSignUpDto } from '../../../test/data'
-import { userModel } from '../../user'
+import request from 'supertest';
+import { validChangePasswordDto, validSignUpDto } from '../../../test/data';
+import { userModel } from '../../user';
 
 describe('/auth', () => {
 	describe('PATCH /change-password', () => {
-		it('Allows an authenticated user change their password as long as they know their current password and choose a valid new one', async () => {
+		it('Allows an authenticated user to change their password as long as they know their current password and choose a valid new one', async () => {
 			expect(validChangePasswordDto.password).not.toBe(
 				validChangePasswordDto.newPassword
-			)
+			);
 
-			const credentials = await global.authenticate(validSignUpDto)
+			const credentials = await global.authenticate(validSignUpDto);
 			const response = await request(global.app)
 				.patch('/auth/change-password')
 				.set('Cookie', ['accessToken=' + credentials.accessToken.value])
-				.send(validChangePasswordDto)
+				.send(validChangePasswordDto);
 
-			expect(response.status).toBe(200)
-			const user = await userModel.findOne({ email: validSignUpDto.email })
+			expect(response.status).toBe(200);
+			const user = await userModel.findOne({ email: validSignUpDto.email });
 			expect(
 				user &&
 					(await user.isPasswordValid(validChangePasswordDto.newPassword, {
-						throwIfInvalid: false
+						throwIfInvalid: false,
 					}))
-			).toBe(true)
-		})
-	})
-})
+			).toBe(true);
+		});
+	});
+});
--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,55 +1,55 @@
-import mongoose, { Mongoose } from 'mongoose'
-import { Express } from 'express'
-import env from '../env'
-import main from '..'
-import { UserDocument } from '../app/user'
-import { authenticate, mockSignUp } from './helpers'
-import { SignInDto, SignUpDto } from '../app/auth'
+import mongoose, { Mongoose } from 'mongoose';
+import { Express } from 'express';
+import env from '../env';
+import main from '..';
+import { UserDocument } from '../app/user';
+import { authenticate, mockSignUp } from './helpers';
+import { SignInDto, SignUpDto } from '../app/auth';
 
 beforeAll(async () => {
 	const connection = await mongoose.connect(env.db.mongoUri, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useCreateIndex: true,
-		useFindAndModify: false
-	})
+		useFindAndModify: false,
+	});
 
-	const app = await main()
-	global.app = app
-	global.connection = connection
-})
+	const app = await main();
+	global.app = app;
+	global.connection = connection;
+});
 
 beforeEach(async () => {
-	const { collections } = mongoose.connection
+	const { collections } = mongoose.connection;
 	for (const collection of Object.values(collections)) {
-		await collection.deleteMany({})
+		await collection.deleteMany({});
 	}
-})
+});
 
 afterAll(async () => {
-	await mongoose.connection.close()
-})
+	await mongoose.connection.close();
+});
 
-global.mockSignUp = mockSignUp
-global.authenticate = authenticate
+global.mockSignUp = mockSignUp;
+global.authenticate = authenticate;
 
 declare global {
 	namespace NodeJS {
 		interface Global {
-			app: Express
-			connection: Mongoose
-			mockSignUp(dto: SignUpDto): Promise<UserDocument>
+			app: Express;
+			connection: Mongoose;
+			mockSignUp(dto: SignUpDto): Promise<UserDocument>;
 			authenticate(
 				dto: SignUpDto | SignInDto,
 				type?: 'sign-in' | 'sign-up'
-			): Promise<{ accessToken: Cookie; refreshToken: Cookie }>
+			): Promise<{ accessToken: Cookie; refreshToken: Cookie }>;
 		}
 	}
 }
 
 type Cookie = {
-	value: string
+	value: string;
 	flags: {
-		HttpOnly: boolean
-	}
-}
+		HttpOnly: boolean;
+	};
+};
